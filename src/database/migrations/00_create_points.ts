d0c82919ed21d2e0cf42e06d3edae0b255625543
saymonp <saymon.t@outlook.com>
2020-06-03 14:56:18 -0300

    Relacionamento points e items

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -7,6 +7,7 @@ export async function up(knex: Knex) {
         table.string("name").notNullable();
         table.string("email").notNullable();
         table.string("whatsapp").notNullable();
+        table.decimal("latitude").notNullable();
         table.decimal("longitude").notNullable();
         table.string("city").notNullable();
         table.string("uf", 2).notNullable();
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,52 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (reqquest, response) => {
-    return response.json({ message: "Hello World" });
+routes.get("/items", async (request, response) => {
+    const items = await knex("items").select("*");
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.name,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    });
+
+    return response.json(serializedItems);
 });
+
+routes.post("/points", async (request, response) => {
+    const {
+        name, email, whatsapp, latitude, longitude, city, uf, items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx("points").insert({
+        image: "image-fake", 
+        name, 
+        email, 
+        whatsapp, 
+        latitude, 
+        longitude, 
+        city, 
+        uf,
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        };
+    })
+
+    await trx("point_items").insert(pointItems);
+
+    return response.json({ sucess: "true" })
+})
 
 export default routes;
